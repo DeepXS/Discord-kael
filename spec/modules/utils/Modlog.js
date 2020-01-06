66ec224d5c0a81dbf0bb63621bf72884db5502b5
Eleven meus seguidores <46073296+Releven@users.noreply.github.com>
2020-01-06 09:22:36 -0300

    Delete Modlog.js

--- spec/modules/utils/Modlog.js
+++
@@ -1,8 +0,0 @@
-import Module from '../../structures/Module.js'
-import Server from '../../server/Server'
-
-export default class extends Module {
-    static run() {
-        Server.write()
-    }
-}
